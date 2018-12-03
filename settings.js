f3ceed27a694d946430d53a3b7e93d6e5c4b7306
Mihó Dániel <miho.daniel@mdy.hu>
2018-12-03 13:33:39 +0100

    Init settings page

---
+++ settings.js
@@ -0,0 +1,9 @@
+var settings = function () {
+	let bankName = 'Bank';
+	let bankURL = '';
+	let btnSettings = document.getElementById('btn-settings');
+	
+	btnSettings.addEventListener('click', () => {
+		alert('Hey, you clicked the settings button!');
+	});
+}();
